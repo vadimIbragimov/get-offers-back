9708c7f47bb11739c5862ccef47179c5e28c778f
ibragimov.v.r <ibragimov.v.r@yandex.ru>
2020-12-21 21:51:54 +0500

    1

--- src/puppeteer/new/groupParsers/stremobzorstore.ts
+++ src/puppeteer/new/groupParsers/stremobzorstore.ts
@@ -3,10 +3,10 @@ import {filterObjectType} from "../tools/filter";
 export const stremObzorStore = (elements: Element[]) => {
     const data = [];
     for (const el of elements){
-        let texthtml: string = el.querySelector('.wall_post_text').innerHTML;
+        const texthtml: string = el.querySelector('.wall_post_text').innerHTML;
         let newtext = '';
-        let br = /<br>/gi;
-        let newStr = texthtml.replace(br, ' ');
+        const br = /<br>/gi;
+        const newStr = texthtml.replace(br, ' ');
         function lookforprice(texthtml: any){
             let numEl: number | string = '';
             if(parseInt(texthtml.match(/\d{5}/)) ){
@@ -28,7 +28,7 @@ export const stremObzorStore = (elements: Element[]) => {
             if (texthtml[i] === '/'  ){
                 data.push({
                     text: newStr,
-                    data: (<HTMLElement>el.querySelector('.rel_date')).innerText,
+                    data: (el.querySelector('.rel_date') as HTMLElement).innerText,
                     price: lookforprice(texthtml),
                     customer: 'https://vk.com' + el.querySelector('.wall_signed_by').getAttribute("href"),
                     post: 'https://vk.com' + el.querySelector('.post_image').getAttribute("href") + '?w=wall' + el.querySelector('.author').getAttribute('data-post-id')
--- src/puppeteer/new/groupParsers/theMarket.ts
+++ src/puppeteer/new/groupParsers/theMarket.ts
@@ -3,10 +3,10 @@ import {filterObjectType} from "../tools/filter";
 export const theMarket = (elements: Element[]) => {
     const data = [];
     for (const el of elements){
-        let texthtml: string = el.querySelector('.wall_post_text').innerHTML;
+        const texthtml: string = el.querySelector('.wall_post_text').innerHTML;
         let newtext = '';
         function lookforprice(texthtml: any){
-            var numEl: number | string = '';
+            let numEl: number | string = '';
             if(parseInt(texthtml.match(/\d{5}/)) ){
                 numEl = parseInt(texthtml.match(/\d{5}/));
             }
@@ -26,7 +26,7 @@ export const theMarket = (elements: Element[]) => {
             if (texthtml[i] === '<' && texthtml[i+1] === 'a'){
                 data.push({
                     text: newtext,
-                    data: (<HTMLElement>el.querySelector('.rel_date')).innerText,
+                    data: (el.querySelector('.rel_date') as HTMLElement).innerText,
                     price: lookforprice(newtext),
                     customer: '-',
                     post: 'https://vk.com' + el.querySelector('.post_image').getAttribute("href") + '?w=wall' + el.querySelector('.author').getAttribute('data-post-id')
