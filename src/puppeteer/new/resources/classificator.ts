fb0259cc79fb2118696fbfca4659a05a18b83055
gpetrov <g.petrov@artsofte.ru>
2021-01-22 09:14:24 +0500

    classificator.ts

---
+++ src/puppeteer/new/resources/classificator.ts
@@ -0,0 +1,69 @@
+export const classificator = [
+    {
+        id: 0,
+        name: 'Adidas',
+        regexp: /adidas|adids|abibas|adedas|адидас/gi,
+        entries: [
+            {
+                id: 1,
+                name: 'Yeezy',
+                regexp: /yeezy|yezy|yeezzy|yezzy|изи|kanye west|kane west|boost|ezzy/gi,
+            },
+            // {
+            //     id: 2,
+            //     name: 'AirMax',
+            //     regexp: /airmax|air max|эир макс|аир макс/gi,
+            // },
+            // {
+            //     id: 2,
+            //     name: 'OffWhite',
+            //     regexp: /off white|of wite|off wite|of white|of|офф вайт/gi,
+            // },
+            // {
+            //     id: 2,
+            //     name: 'SB',
+            //     regexp: /SB/gi,
+            // },
+            // {
+            //     id: 2,
+            //     name: 'Air Force',
+            //     regexp: /air force|air forc|air foce|аир форс|Af/gi,
+            // },
+        ],
+        
+    },
+    {
+        id: 1,
+        name: 'Nike',
+        regexp: /nike|nik|n*ke|найк|nake/gi,
+        entries: [
+            {
+                id: 1,
+                name: 'Jordan',
+                regexp: /jordan|джордан|жоры|airjordan|jordn|.jordan|жордан|jordon/gi,
+            },
+            {
+                id: 2,
+                name: 'AirMax',
+                regexp: /airmax|air max|эир макс|аир макс/gi,
+            },
+            {
+                id: 2,
+                name: 'OffWhite',
+                regexp: /off white|of wite|off wite|of white|of|офф вайт/gi,
+            },
+            {
+                id: 2,
+                name: 'SB',
+                regexp: /SB/gi,
+            },
+            {
+                id: 2,
+                name: 'Air Force',
+                regexp: /air force|air forc|air foce|аир форс|Af/gi,
+            },
+        ],
+        
+    }
+
+];
