cbd959640c6231e2d01f1c2aa2223dcf148fb554
Petrov012110 <grisha14.06.96@gmail.com>
2021-01-23 21:37:19 +0500

    new items in classificator

--- src/puppeteer/new/resources/classificator.ts
+++ src/puppeteer/new/resources/classificator.ts
@@ -5,15 +5,30 @@ export const classificator = [
         regexp: /adidas|adids|abibas|adedas|адидас/gi,
         entries: [
             {
-                id: 1,
+                id: 0,
                 name: 'Yeezy',
                 regexp: /yeezy|yezy|yeezzy|yezzy|изи|kanye west|kane west|boost|ezzy/gi,
             },
-            // {
-            //     id: 2,
-            //     name: 'AirMax',
-            //     regexp: /airmax|air max|эир макс|аир макс/gi,
-            // },
+            {
+                id: 1,
+                name: '350v2',
+                regexp: /350|v2|350v2/gi,
+            },
+            {
+                id: 2,
+                name: 'Y-3',
+                regexp: /y-3|y3/gi,
+            },
+            {
+                id: 3,
+                name: 'Raf Simons',
+                regexp: /raf simons|by raf simons|raf|simons|раф/gi,
+            },
+            {
+                id: 4,
+                name: 'Alexander Wang',
+                regexp: /alexander wang|wang/gi,
+            },
             // {
             //     id: 2,
             //     name: 'OffWhite',
@@ -38,12 +53,12 @@ export const classificator = [
         regexp: /nike|nik|n*ke|найк|nake/gi,
         entries: [
             {
-                id: 1,
+                id: 0,
                 name: 'Jordan',
                 regexp: /jordan|джордан|жоры|airjordan|jordn|.jordan|жордан|jordon/gi,
             },
             {
-                id: 2,
+                id: 1,
                 name: 'AirMax',
                 regexp: /airmax|air max|эир макс|аир макс/gi,
             },
@@ -53,17 +68,244 @@ export const classificator = [
                 regexp: /off white|of wite|off wite|of white|of|офф вайт/gi,
             },
             {
-                id: 2,
+                id: 3,
                 name: 'SB',
                 regexp: /SB/gi,
             },
             {
-                id: 2,
+                id: 4,
                 name: 'Air Force',
                 regexp: /air force|air forc|air foce|аир форс|Af/gi,
             },
+            {
+                id: 5,
+                name: 'Air VaporMax',
+                regexp: /air vapormax|vapomax/gi,
+            },
+            {
+                id: 6,
+                name: 'Suptempo',
+                regexp: /suptempo/gi,
+            },
         ],
         
-    }
+    },
+    {
+        id: 2,
+        name: 'Gucci',
+        regexp: /gucci|гучи|guci/gi,
+        entries: [
+            {
+                id: 0,
+                name: 'logo bag',
+                regexp: /logo|bag/gi,
+            },
+        ],
+        
+    },
+    {
+        id: 3,
+        name: 'Stone Island',
+        regexp: /stone|island|стон|ston/gi,
+        entries: [
+            // {
+            //     id: 0,
+            //     name: 'logo bag',
+            //     regexp: /logo|bag/gi,
+            // },
+        ],
+        
+    },
+    {
+        id: 4,
+        name: 'Travis Scott',
+        regexp: /Travis|Travis Scott/gi,
+        entries: [
+            // {
+            //     id: 0,
+            //     name: 'logo bag',
+            //     regexp: /logo|bag/gi,
+            // },
+        ],
+        
+    },
+    {
+        id: 4,
+        name: 'Off-White',
+        regexp: /Off white|off-white/gi,
+        entries: [
+            {
+                id: 0,
+                name: 'belt',
+                regexp: /belt|ремень|industrial/gi,
+            },
+        ],
+        
+    },
+    {
+        id: 5,
+        name: 'The North Face',
+        regexp: /thenorthface|tnf|thenorthface|тнф/gi,
+        entries: [
+            // {
+            //     id: 0,
+            //     name: 'belt',
+            //     regexp: /belt|ремень|industrial/gi,
+            // },
+        ],
+        
+    },
+    {
+        id: 6,
+        name: 'Rick Owens',
+        regexp: /rickowens|rick owens/gi,
+        entries: [
+            {
+                id: 0,
+                name: 'Ramones',
+                regexp: /ramones|рамоны/gi,
+            },
+        ],
+    },
+    {
+        id: 7,
+        name: 'Heron Preston',
+        regexp: /heron preston|preston|херон престон/gi,
+        entries: [
+            // {
+            //     id: 0,
+            //     name: 'Ramones',
+            //     regexp: /ramones|рамоны/gi,
+            // },
+        ],
+    },
+    {
+        id: 8,
+        name: 'Calvin Klein',
+        regexp: /calvin klein|ck/gi,
+        entries: [
+            {
+                id: 0,
+                name: 'Logo',
+                regexp: /logo|лого/gi,
+            },
+            {
+                id: 1,
+                name: 'Jeans',
+                regexp: /jeans/gi,
+            },
+        ],
+    },
+    {
+        id: 9,
+        name: 'Supreme',
+        regexp: /supreme|суприм|suprem/gi,
+        entries: [
+            {
+                id: 0,
+                name: 'Box-Logo',
+                regexp: /logo|лого|box logo|box|bogo|бого|sweatshirt|hooded/gi,
+            },
+        ],
+    },
+    {
+        id: 10,
+        name: 'Burberry',
+        regexp: /burberry/gi,
+        entries: [
+            // {
+            //     id: 0,
+            //     name: 'Box-Logo',
+            //     regexp: /logo|лого|box logo|box|bogo|бого/gi,
+            // },
+        ],
+    },
+    {
+        id: 11,
+        name: 'Lacoste',
+        regexp: /lacoste|лакост|lacost/gi,
+        entries: [
+            // {
+            //     id: 0,
+            //     name: 'Box-Logo',
+            //     regexp: /logo|лого|box logo|box|bogo|бого/gi,
+            // },
+        ],
+    },
+    {
+        id: 12,
+        name: 'Balenciaga',
+        regexp: /balenciaga|balensiaga/gi,
+        entries: [
+            {
+                id: 0,
+                name: 'tee',
+                regexp: /tee|t shirt|t-shirt|тишка/gi,
+            },
+        ],
+    },
+    {
+        id: 13,
+        name: 'Kenzo',
+        regexp: /kenzo|кензо/gi,
+        entries: [
+            {
+                id: 0,
+                name: 'tee',
+                regexp: /tee|t shirt|t-shirt|тишка/gi,
+            },
+        ],
+    },
+    {
+        id: 14,
+        name: 'Dolce Gabbana',
+        regexp: /Dolce Gabbana|Gabbana|Dolce/gi,
+        entries: [
+            // {
+            //     id: 0,
+            //     name: 'tee',
+            //     regexp: /tee|t shirt|t-shirt|тишка/gi,
+            // },
+        ],
+    },
+    {
+        id: 15,
+        name: 'Haliky',
+        regexp: /Haliky|Dolce/gi,
+        entries: [
+            // {
+            //     id: 0,
+            //     name: 'tee',
+            //     regexp: /tee|t shirt|t-shirt|тишка/gi,
+            // },
+        ],
+    },
+    {
+        id: 16,
+        name: 'Bape',
+        regexp: /bape/gi,
+        entries: [
+            // {
+            //     id: 0,
+            //     name: 'tee',
+            //     regexp: /tee|t shirt|t-shirt|тишка/gi,
+            // },
+        ],
+    },
+    {
+        id: 17,
+        name: 'stussy',
+        regexp: /stussy/gi,
+        entries: [
+            // {
+            //     id: 0,
+            //     name: 'tee',
+            //     regexp: /tee|t shirt|t-shirt|тишка/gi,
+            // },
+        ],
+    },
+    
+    
+    
 
 ];
