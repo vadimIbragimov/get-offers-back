cc13722f0650aeae0637fbd158c8caafa98e5960
ibragimov.v.r <ibragimov.v.r@yandex.ru>
2021-03-10 23:06:52 +0500

    fix

--- src/puppeteer/resources/groups.ts
+++ src/puppeteer/resources/groups.ts
@@ -1,58 +1,58 @@
 export const groupsList = [
-    // {
-    //     name: 'theMarket',
-    //     href: 'tmrkt',
-    // },
-    // {
-    //     name: 'stremobzorstore',
-    //     href: 'stremobzorstore',
-    // },
+    {
+        name: 'theMarket',
+        href: 'tmrkt',
+    },
+    {
+        name: 'stremobzorstore',
+        href: 'stremobzorstore',
+    },
     {
         name: 'shmotki',
         href: 'shmotki_shmotochki',
     },
-    // {
-    //     name: 'getsneakers',
-    //     href: 'getsneakers',
-    // },
-    // {
-    //     name: 'krossovkee',
-    //     href: 'krossovkee',
-    // },
-    // {
-    //     name: 'stockmsk',
-    //     href: 'stockmsk',
-    // },
-    // {
-    //     name: 'stocksaintpetersburg',
-    //     href: 'stocksaintpetersburg',
-    // },
-    // {
-    //     name: 'hype35',
-    //     href: 'hype35',
-    // },
-    // {
-    //     name: 'fa_sales',
-    //     href: 'fa_sales',
-    // },
-    // {
-    //     name: 'sneakersale',
-    //     href: 'sneakersale',
-    // },
-    // {
-    //     name: 'baraholkacasualnaya',
-    //     href: 'baraholkacasualnaya',
-    // },
-    // {
-    //     name: 'marktplc',
-    //     href: 'marktplc',
-    // },
-    // {
-    //     name: 'brahand',
-    //     href: 'brahand',
-    // },
-    // {
-    //     name: 'resellpoint',
-    //     href: 'resellpoint',
-    // },
+    {
+        name: 'getsneakers',
+        href: 'getsneakers',
+    },
+    {
+        name: 'krossovkee',
+        href: 'krossovkee',
+    },
+    {
+        name: 'stockmsk',
+        href: 'stockmsk',
+    },
+    {
+        name: 'stocksaintpetersburg',
+        href: 'stocksaintpetersburg',
+    },
+    {
+        name: 'hype35',
+        href: 'hype35',
+    },
+    {
+        name: 'fa_sales',
+        href: 'fa_sales',
+    },
+    {
+        name: 'sneakersale',
+        href: 'sneakersale',
+    },
+    {
+        name: 'baraholkacasualnaya',
+        href: 'baraholkacasualnaya',
+    },
+    {
+        name: 'marktplc',
+        href: 'marktplc',
+    },
+    {
+        name: 'brahand',
+        href: 'brahand',
+    },
+    {
+        name: 'resellpoint',
+        href: 'resellpoint',
+    },
 ];
